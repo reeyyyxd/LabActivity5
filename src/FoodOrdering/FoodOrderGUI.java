b2b100640a3939ac3d09c2d2eb15081407f646bd
reeyyyxd <2woaranthony@gmail.com>
2022-12-17 21:56:34 +0800

    Rename FoodOrder components Add FoodOrder main function Complete FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -20,12 +20,86 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb15;
 
 
-    public FoodOrderGUI() {
+    public FoodOrderGUI(){
+        btnOrder.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    ButtonGroup group = new ButtonGroup();
+                    group.add(rbNone);
+                    group.add(rb5);
+                    group.add(rb10);
+                    group.add(rb15);
+
+                    boolean selectbox = false;
+                    double price = 0;
+
+
+                    if (cPizza.isSelected()) {
+                        selectbox= true;
+                        price += 100;
+
+                    }
+                    if (cBurger.isSelected()) {
+                        selectbox = true;
+                        price += 80;
+
+                    }
+                    if (cFries.isSelected()) {
+                        selectbox = true;
+                        price += 65;
+                    }
+                    if (cSoftDrinks.isSelected()) {
+                        selectbox= true;
+                        price += 55;
+
+                    }
+                    if (cTea.isSelected()) {
+                        selectbox = true;
+                        price += 50;
+
+                    }
+                    if (cSundae.isSelected()) {
+                        selectbox = true;
+                        price += 40;
+
+                    }
+
+                    if (rbNone.isSelected()) {
+                        selectbox = true;
+                        price = price;
+
+                    }
+                    if (rb5.isSelected()) {
+                        selectbox = true;
+                        double disprice = price * 0.05;
+                        price = price - disprice;
+
+                    }
+                    if (rb10.isSelected()) {
+                        selectbox = true;
+                        double disprice = price * 0.10;
+                        price = price - disprice;
+
+                    }
+                    if (rb15.isSelected()) {
+                        selectbox = true;
+                        double  disprice = price * 0.15;
+                        price = price - disprice;
+
+                    }
+
+
+            }
+        });
     }
 
 
     public static void main(String[] args) {
-
+        FoodOrderGUI app = new FoodOrderGUI();
+        app.setTitle("Food Ordering System");
+        app.setContentPane(app.OrderPanel);
+        app.setSize(1280, 720);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
     }
 }
-
