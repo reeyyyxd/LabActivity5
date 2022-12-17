ffa8f47f4ae90056e44e8c9da8489ab99a02d5d1
reeyyyxd <2woaranthony@gmail.com>
2022-12-17 22:01:28 +0800

    Rename LeapYear components Add LeapYear main function Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -9,13 +9,35 @@ public class LeapYearGUI extends JFrame {
     private JButton btnCheckYear;
 
     public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String inputyear = tfYear.getText();
+                    int year = Integer.parseInt(inputyear);
+                    if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)){
+                        JOptionPane.showMessageDialog(LeapPanel, "Leap Year");
+                    }
+                    if (year < 0) {
+                        throw new IllegalArgumentException("Years not be negative");
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(LeapPanel, "Not a Leap Year");
+                    }
 
+
+            }
+        });
     }
 
 
 
 
         public static void main(String[] args) {
-
+        LeapYearGUI app = new LeapYearGUI();
+        app.setTitle("Leap Year Checker");
+        app.setContentPane(app.LeapPanel);
+        app.setSize(1280, 720);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
     }
 }
