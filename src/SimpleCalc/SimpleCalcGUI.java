0d3c53c0c90f95c9c6da6ef509e9c1879770d72b
reeyyyxd <2woaranthony@gmail.com>
2022-12-17 22:02:46 +0800

    Rename SimpleCalc components

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,63 +3,11 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SimpleCalcGUI extends JFrame {
+public class SimpleCalcGUI {
     private JPanel CalcuPanel;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
-
-    public SimpleCalcGUI(){
-
-        btnCompute.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    String input1 = tfNumber1.getText();
-                    int inputNum1 = Integer.parseInt(input1);
-
-                    String input2 = tfNumber2.getText();
-                    int inputNum2 = Integer.parseInt(input2);
-                    int equation = 0;
-                    String operator = (String) cbOperations.getSelectedItem();
-
-
-                    switch (operator) {
-                        case "+":
-                            equation = inputNum1 + inputNum2;
-                            break;
-                        case "-":
-                            equation = inputNum1 - inputNum2;
-                            break;
-                        case "*":
-                            equation = inputNum1 * inputNum2;
-                            break;
-                        case "/":
-                            equation = inputNum1 / inputNum2;
-                            break;
-                    }
-                    lblResult.setText(Integer.toString(equation));
-                }catch(NumberFormatException f){
-                    JOptionPane.showMessageDialog(null, "Wrong input!");
-                }catch(NullPointerException h) {
-                    JOptionPane.showMessageDialog(null, "Wrong input!");
-                }catch(ArithmeticException g){
-                    JOptionPane.showMessageDialog(null, "Cannot divide zero!");
-                }
-            }
-        });
-    }
-
-    public static void main(String[] args) {
-
-        SimpleCalcGUI app = new SimpleCalcGUI();
-        app.setTitle("Simple Calculator");
-        app.setContentPane(app.CalcuPanel);
-        app.setSize(1280, 720);
-        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        app.setVisible(true);
-        app.lblResult.setEditable(false);
-
-    }
 }
