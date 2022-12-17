eba881a5c329b70181d9a0c89c23eb3043488f98
reeyyyxd <2woaranthony@gmail.com>
2022-12-17 22:03:59 +0800

    Rename SimpleCalc components Add SimpleCalc main function Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -13,12 +13,47 @@ public class SimpleCalcGUI extends JFrame {
 
     public SimpleCalcGUI(){
 
+        btnCompute.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String input1 = tfNumber1.getText();
+                    int inputNum1 = Integer.parseInt(input1);
 
+                    String input2 = tfNumber2.getText();
+                    int inputNum2 = Integer.parseInt(input2);
+                    int equation = 0;
+                    String operator = (String) cbOperations.getSelectedItem();
+
+
+                    switch (operator) {
+                        case "+":
+                            equation = inputNum1 + inputNum2;
+                            break;
+                        case "-":
+                            equation = inputNum1 - inputNum2;
+                            break;
+                        case "*":
+                            equation = inputNum1 * inputNum2;
+                            break;
+                        case "/":
+                            equation = inputNum1 / inputNum2;
+                            break;
+                    }
+                    lblResult.setText(Integer.toString(equation));
+
+            }
+        });
     }
 
     public static void main(String[] args) {
 
-
+        SimpleCalcGUI app = new SimpleCalcGUI();
+        app.setTitle("Simple Calculator");
+        app.setContentPane(app.CalcuPanel);
+        app.setSize(1280, 720);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.lblResult.setEditable(false);
 
     }
 }
